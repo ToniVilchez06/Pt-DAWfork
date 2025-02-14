8379651aed8c970cfb8967fe4d8d86251b920f0f
Toni <tonivilchezortega@gmail.com>
2025-02-14 20:38:14 +0100

    Modificacio 2

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ class Program
     static void Main(string[] args)
     {
         // Crear un objecte Vehicle
-        Vehicle vehicle1 = new Vehicle("M4", "BMW", 2008, 200.0);
+        Vehicle vehicle1 = new Vehicle("Cooper", "Mini", 2020, 220.0);
 
         // Mostrar la informació del vehicle
         vehicle1.MostrarInformacio();
 
         // Crear un altre objecte Vehicle
-        Vehicle vehicle2 = new Vehicle("Picanto", "Kia", 2022, 190.0);
+        Vehicle vehicle2 = new Vehicle("A3", "Audi", 2006, 210.0);
         
         // Mostrar la informació del segon vehicle
         vehicle2.MostrarInformacio();
