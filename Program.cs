687b0fce951fe5c57bbef7e841e6b18874c47135
Toni <tonivilchezortega@gmail.com>
2025-02-14 21:21:07 +0100

    Error dotnet build solucionat

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@ class Program
     static void Main(string[] args)
     {
         // Crear un objecte Vehicle
-        Vehicle vehicle1 = new Vehicle("Cooper", "Mini", 2020, 220.0);
+        Cotxe cotxe1 = new Cotxe("Cooper", "Mini", 2020, 220.0, 5);
 
         // Mostrar la informació del vehicle
-        vehicle1.MostrarInformacio();
+        cotxe1.MostrarInformacio();
 
         // Crear un altre objecte Vehicle
-        Vehicle vehicle2 = new Vehicle("A3", "Audi", 2006, 210.0);
+        Cotxe cotxe2 = new Cotxe("A3", "Audi", 2006, 210.0, 3);
         
         // Mostrar la informació del segon vehicle
-        vehicle2.MostrarInformacio();
+        cotxe2.MostrarInformacio();
     }
 }
