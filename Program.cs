59e2fe2c9b714c419b8c72d1d0d0154c1579ad49
Toni <tonivilchezortega@gmail.com>
2025-02-14 23:57:24 +0100

    Modificacio codi Program.cs i creacio de la pagina index.html dins de la carpeta Vista

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
+<<<<<<< Updated upstream
         // Crear un objecte Vehicle
         Vehicle vehicle1 = new Vehicle("Cooper", "Mini", 2020, 220.0);
 
@@ -16,5 +17,44 @@ class Program
         
         // Mostrar la informació del segon vehicle
         vehicle2.MostrarInformacio();
+=======
+         // Crear llistes per a cada tipus de vehicle
+        List<Vaixell> vaixells = new List<Vaixell>
+        {
+            new Vaixell("Explorer", "OceanX", 2015, 45, 12000),
+            new Vaixell("Voyager", "BlueWave", 2020, 60, 15000)
+        };
+
+        List<Cotxe> cotxes = new List<Cotxe>
+        {
+            new Cotxe("Model S", "Tesla", 2022, 250, 4),
+            new Cotxe("Civic", "Honda", 2019, 220, 4)
+        };
+
+        List<Camio> camions = new List<Camio>
+        {
+            new Camio("Actros", "Mercedes", 2021, 120, 18000),
+            new Camio("FH16", "Volvo", 2018, 110, 25000)
+        };
+
+        // Mostrar informació de cada tipus de vehicle
+        Console.WriteLine("Llista de Vaixells:");
+        foreach (var vaixell in vaixells)
+        {
+            vaixell.MostrarInformacio();
+        }
+
+        Console.WriteLine("\nLlista de Cotxes:");
+        foreach (var cotxe in cotxes)
+        {
+            cotxe.MostrarInformacio();
+        }
+
+        Console.WriteLine("\nLlista de Camions:");
+        foreach (var camio in camions)
+        {
+            camio.MostrarInformacio();
+        }
+>>>>>>> Stashed changes
     }
 }
